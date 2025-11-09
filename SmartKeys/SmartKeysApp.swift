2281335146e5eb5894a2ee75a4c539cfbfbf7b0a
inazumimakoto <inazumimakoto@gmail.com>
2025-11-09 20:25:30 +0900

    Initial Commit

---
+++ SmartKeys/SmartKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartKeysApp.swift
+//  SmartKeys
+//
+//  Created by inazumimakoto on 2025/11/09.
+//
+
+import SwiftUI
+
+@main
+struct SmartKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
